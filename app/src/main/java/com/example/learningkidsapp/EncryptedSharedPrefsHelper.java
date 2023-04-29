c2fdde7671f6e095893878d6edd51522203efd12
AndyK1202 <andykhounnoraj@my.untdallas.edu>
2023-04-29 17:16:31 -0500

    Added Shared Encryption for One time login

---
+++ app/src/main/java/com/example/learningkidsapp/EncryptedSharedPrefsHelper.java
@@ -0,0 +1,55 @@
+package com.example.learningkidsapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class EncryptedSharedPrefsHelper {
+
+    private static final String SHARED_PREFS_FILE_NAME = "com.example.learningkidsapp.SHARED_PREFS";
+    private static final String AUTH_TOKEN_KEY = "auth_token";
+
+    private SharedPreferences encryptedSharedPreferences;
+
+    public EncryptedSharedPrefsHelper(Context context) {
+        try {
+            MasterKey masterKey = new MasterKey.Builder(context)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            encryptedSharedPreferences = EncryptedSharedPreferences.create(
+                    context,
+                    SHARED_PREFS_FILE_NAME,
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isLoggedIn() {
+        return encryptedSharedPreferences.contains(AUTH_TOKEN_KEY);
+    }
+
+    public void saveAuthToken(String authToken) {
+        encryptedSharedPreferences.edit().putString(AUTH_TOKEN_KEY, authToken).apply();
+    }
+
+    public String getAuthToken() {
+        return encryptedSharedPreferences.getString(AUTH_TOKEN_KEY, null);
+    }
+
+    public void removeAuthToken() {
+        encryptedSharedPreferences.edit().remove(AUTH_TOKEN_KEY).apply();
+    }
+}
+
+
+
