325a5a0e26217f7fae689578d944ffe586a8663f
diegoluci <alemandiego25@gmail.com>
2023-04-29 18:22:15 -0500

    Substring Test

--- app/src/main/java/com/example/learningkidsapp/PhoneGame.java
+++ app/src/main/java/com/example/learningkidsapp/PhoneGame.java
@@ -2,7 +2,10 @@ package com.example.learningkidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PhoneGame extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class PhoneGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_game);
+        TextView test;
+        test = findViewById(R.id.Digits);
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+        String phone = sp.getString("Phone", "");
+        String shortphone = phone.substring(0,3);
+        test.setText(shortphone);
     }
 }
