26173d469c4faf0e2396032b5262f011dec9894f
Andy Khounnoraj <113858418+AndyK1202@users.noreply.github.com>
2023-10-17 22:33:14 -0500

    Add files via upload

--- app/src/main/java/com/example/learningkidsapp/Settings.java
+++ app/src/main/java/com/example/learningkidsapp/Settings.java
@@ -11,11 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Settings extends AppCompatActivity {
 
         //initializing variables
         EditText fname, lname, phone, address;
-        Button save, home;
+        Button save, home, btnLogout;
         SharedPreferences sp;
         String fnameStr, lnameStr, phoneStr, addrStr;
 
@@ -31,6 +37,7 @@ public class Settings extends AppCompatActivity {
         address = findViewById(R.id.Address);
         save = findViewById(R.id.Save);
         home = findViewById(R.id.Home);
+        btnLogout = findViewById(R.id.btnLogout);
 
         //Declaring Shared Preferences
        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
@@ -63,6 +70,35 @@ public class Settings extends AppCompatActivity {
                 startActivity(intentH);
             }
         });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Clear any session-related data here, if needed
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();  // Clear all stored data
+                editor.commit(); // Commit the changes
+
+                // Optional: If you're using Firebase Authentication, sign out from Firebase
+                // FirebaseAuth.getInstance().signOut();
+
+                // Create a new intent to start the LoginActivity
+                Intent intent = new Intent(Settings.this, LoginActivity.class);
+
+                // These flags will cause the LoginActivity to become the start of a new task on this history stack.
+                // It should clear any old activities (including the game home screen) from the stack, ensuring that
+                // the LoginActivity is the only thing on the stack. The user will not be able to go back to the game home screen
+                // from the login screen by pressing the back button.
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                // Start the new activity
+                startActivity(intent);
+
+                // Call finish() on the current activity
+                finish();  // This closes the current activity, preventing the user from navigating back to it
+            }
+        });
+
 
     }
 }
